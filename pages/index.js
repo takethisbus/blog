3ae8f1de4ca54faf2af1f4ccd6d3dd59bc2eacfe
jinajeon <jina.jeon@buildblock.io>
2022-09-04 19:59:10 +0900

    change to ssg with api

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,24 @@ import { useEffect, useState } from 'react';
 import Layout, { siteTitle } from '../components/Layout';
 import utilStyles from '../styles/utils.module.css';
 
-// export async function getStaticProps() { 
-//   const allPostsData = getSortedPostsData();
-//   return {
-//     props: {
-//       allPostsData,
-//     }
-//   }
-// }
+export async function getStaticProps() { 
+  const response = await fetch("http://localhost:3000/api/posts");
+  const json = await response.json()
+  
+  return {
+    props: {
+      allPostsData: json.allPostsData,
+    }
+  }
+}
 
-export default function Home() {
-  const [allPostsData, setAllPostsData] = useState([]);
-  useEffect(() => { 
-    fetch("/api/posts")
-      .then(res => res.json())
-      .then(data => setAllPostsData(data.allPostsData))
-  },[])
+export default function Home({ allPostsData }) {
+  // const [allPostsData, setAllPostsData] = useState([]);
+  // useEffect(() => { 
+  //   fetch("/api/posts")
+  //     .then(res => res.json())
+  //     .then(data => setAllPostsData(data.allPostsData))
+  // },[])
   return (
     <Layout home>
       <Head>
