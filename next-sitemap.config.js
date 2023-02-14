a3af9f9768e0dbb119eed63065261ea8d81e9bca
jinajeon <jina.jeon@buildblock.io>
2023-02-14 17:41:45 +0900

    test

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,5 @@
  /** @type {import('next-sitemap').IConfig} */
+// eslint-disable-next-line no-undef
 module.exports = {
   siteUrl: "https://blog-jinjinzara.vercel.app/",
   generateRobotsTxt: true
