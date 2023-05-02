8bf9c5f670a4e53f1b753c3f0bbc91af8c0440c9
jinajeon <jina.jeon@buildblock.io>
2023-05-02 18:27:24 +0900

    Delete: useless section

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/Layout';
-import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
-import Date from '../components/Date';
-import { getSortedPostsData } from '../lib/posts';
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/Layout'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import Date from '../components/Date'
+import { getSortedPostsData } from '../lib/posts'
 
-export async function getStaticProps() { 
-  const allPostsData = getSortedPostsData();
-  
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+
   return {
     props: {
-      allPostsData
-    }
+      allPostsData,
+    },
   }
 }
 
@@ -21,13 +21,6 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Blog</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
@@ -43,5 +36,5 @@ export default function Home({ allPostsData }) {
         </ul>
       </section>
     </Layout>
-  );
+  )
 }
