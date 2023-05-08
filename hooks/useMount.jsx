c075bcf0c22119be0ef4e6099631ac0cb3633411
jinajeon <jina.jeon@buildblock.io>
2023-05-08 22:59:59 +0900

    Add: useMount custom hook

---
+++ hooks/useMount.jsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from 'react'
+
+export const useMount = () => {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  return mounted
+}
